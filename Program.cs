1eb19c796d4ab7885d09059c0a186d364efc238d
Mnishthakur <102964888+Mnishthakur@users.noreply.github.com>
2023-06-22 18:34:43 +0530

    [Manish Thakur]: Added 5th program

--- Program.cs
+++ Program.cs
@@ -7,12 +7,21 @@ class Program
 {
     static void Main()
     {
-        string pattern = "fox(es)?";
-        string input = "foxes are omnivorous mammals belonging to several genera of the family Canidae fox.";
+        string input = "image.jpg text.png document.docx code.cs backup.zip";
 
-        MatchCollection matches = Regex.Matches(input, pattern);
-        int count = matches.Count;
+        MatchCollection matches = Regex.Matches(input, @"\b\w+\.(jpg|jpeg|png|gif|bmp)\b");
 
-        Console.WriteLine("There are {0} occurrences.", count);
+        if (matches.Count > 0)
+        {
+            Console.WriteLine("Valid image file names found:");
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match.Value);
+            }
+        }
+        else
+        {
+            Console.WriteLine("No valid image file names found.");
+        }
     }
 }
